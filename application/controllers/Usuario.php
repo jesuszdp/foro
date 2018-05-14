2a5f29410ac8a2d4387ef7b577407ed037b4fcc2
leas88 <cenitluis.pumas@gmail.com>
2018-05-14 15:50:39 -0500

    cambio de catalogo de sexo de usuarios, incidencia que todos los usuarios los mostraba como masculinos

--- application/controllers/Usuario.php
+++ application/controllers/Usuario.php
@@ -7,15 +7,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  *
  * @author chrigarc
  */
-class Usuario extends MY_Controller
-{
+class Usuario extends MY_Controller {
 
     const LIMIT = 10, LISTA = 'lista', BASICOS = 'basico', PASSWORD = 'password',
             NIVELES_ACCESO = 'niveles', NO_SIAP = 'no_siap', SIAP = 'siap', NO_IMSS = 'no_imss',
             STATUS_ACTIVIDAD = 'actividad';
 
-    function __construct()
-    {
+    function __construct() {
+        $this->grupo_language_text = ['registro_usuario'];
         parent::__construct();
         $this->load->library('form_complete');
         $this->load->library('seguridad');
@@ -25,49 +24,47 @@ class Usuario extends MY_Controller
         $this->template->setTitle('Usuarios');
     }
 
-    public function index()
-    {
+    public function index() {
         redirect('usuario/get_usuarios/');
     }
 
-    public function get_usuarios($usuario = '')
-    {
+    public function get_usuarios($usuario = '') {
         $output = [];
-        switch($usuario)
-        {
+        switch ($usuario) {
             case Usuario::LISTA:
                 $get = $this->input->get(null, true);
                 $this->lista_usuarios($get);
                 break;
             case '':
+                $output['language_text'] = $this->language_text;
                 $view = $this->load->view('usuario/get_usuarios_v2.tpl.php', $output, true);
                 $this->template->setMainContent($view);
                 $this->template->getTemplate();
                 break;
             default:
+                pr($output);
                 $this->muestra_usuario($usuario);
                 break;
         }
     }
 
-    private function muestra_usuario($usuario = 0)
-    {
+    private function muestra_usuario($usuario = 0) {
+        $output['language_text'] = $this->language_text;
         $params['where'] = array(
-           'usuarios.id_usuario' => $usuario
+            'usuarios.id_usuario' => $usuario
         );
         $params['select'] = array(
-           'usuarios.id_usuario', 'inf.matricula', 'inf.nombre',
-           'inf.apellido_paterno', 'inf.apellido_materno', 'sexo',
-           'inf.fecha_nacimiento', 'dep.nombre departamento', //'dep.id_departamento_instituto', 
-           'cat.id_categoria', 'cat.nombre categoria', //'C.cve_tipo_contratacion',
-           'inf.curp', 'inf.rfc', 'inf.telefono_personal',
-           'inf.telefono_oficina', 'inf.email', 'usuarios.activo usuario_activo',
-           'dep.clave_departamental', 'cat.clave_categoria'
+            'usuarios.id_usuario', 'inf.matricula', 'inf.nombre',
+            'inf.apellido_paterno', 'inf.apellido_materno', 'sexo',
+            'inf.fecha_nacimiento', 'dep.nombre departamento', //'dep.id_departamento_instituto', 
+            'cat.id_categoria', 'cat.nombre categoria', //'C.cve_tipo_contratacion',
+            'inf.curp', 'inf.rfc', 'inf.telefono_personal',
+            'inf.telefono_oficina', 'inf.email', 'usuarios.activo usuario_activo',
+            'dep.clave_departamental', 'cat.clave_categoria'
         );
         $resultado = $this->usuario->get_usuarios($params);
-        if (count($resultado) == 1)
-        {
-           $output['usuario'] = $resultado[0];
+        if (count($resultado) == 1) {
+            $output['usuario'] = $resultado[0];
         }
         $output['datos_basicos'] = $this->load->view('usuario/datos_basicos.tpl.php', $output, true);
         $output['grupos_usuario'] = $this->usuario->get_niveles_acceso($output['usuario']['id_usuario']);
@@ -79,17 +76,16 @@ class Usuario extends MY_Controller
         $this->template->getTemplate();
     }
 
-    private function lista_usuarios(&$params = [])
-    {
+    private function lista_usuarios(&$params = []) {
 
         $filtros = $this->genera_filtros($params);
-        $filtros['limit'] = isset($params['pageSize'])? $params['pageSize']:Usuario::LIMIT;
-        $filtros['offset'] = isset($params['pageIndex'])?  ($filtros['limit']*($params['pageIndex']-1)):0;
+        $filtros['limit'] = isset($params['pageSize']) ? $params['pageSize'] : Usuario::LIMIT;
+        $filtros['offset'] = isset($params['pageIndex']) ? ($filtros['limit'] * ($params['pageIndex'] - 1)) : 0;
 
         $filtros['select'] = array(
             'usuarios.id_usuario', 'coalesce(inf.matricula, usuarios.username) matricula',
             'concat(inf.nombre, $$ $$, inf.apellido_paterno, $$ $$, inf.apellido_materno) nombre',
-            'del.nombre delegacion', 'uni.nombre unidad', 'inf.es_imss', 'usuarios.activo', 
+            'del.nombre delegacion', 'uni.nombre unidad', 'inf.es_imss', 'usuarios.activo',
             '(select array_agg(nombre) from sistema.roles R join sistema.usuario_rol UR on R.clave_rol=UR.clave_rol and UR.id_usuario=usuarios.id_usuario and UR.activo=true) as rol'
         );
         $usuarios['data'] = $this->usuario->get_usuarios($filtros); //exit();
@@ -101,15 +97,11 @@ class Usuario extends MY_Controller
         echo json_encode($usuarios);
     }
 
-    private function genera_filtros($params)
-    {
+    private function genera_filtros($params) {
         $filtros = [];
-        foreach ($params as $key => $value)
-        {
-            if($value != '')
-            {
-                switch ($key)
-                {
+        foreach ($params as $key => $value) {
+            if ($value != '') {
+                switch ($key) {
                     case 'matricula':
                         $filtros['like']['usuarios.username'] = $value;
                         break;
@@ -133,20 +125,16 @@ class Usuario extends MY_Controller
                         break;
                 }
             }
-
         }
         return $filtros;
     }
 
-    public function editar($id_usuario = 0, $tipo = Usuario::BASICOS)
-    {
+    public function editar($id_usuario = 0, $tipo = Usuario::BASICOS) {
         $salida = [];
         $view = '';
-        if ($this->input->post() && $this->input->is_ajax_request())
-        {
+        if ($this->input->post() && $this->input->is_ajax_request()) {
             $this->config->load('form_validation'); //Cargar archivo con validaciones
-            switch ($tipo)
-            {
+            switch ($tipo) {
                 case Usuario::BASICOS:
                     $view = $this->get_datos_basicos($id_usuario);
                     $validations = $this->config->item('form_actualizar'); //Obtener validaciones de archivo general
@@ -160,25 +148,22 @@ class Usuario extends MY_Controller
                     $view = $this->get_niveles($id_usuario);
                     break;
                 case Usuario::STATUS_ACTIVIDAD:
-                   $validations = $this->config->item('form_status_actividad_usuario');
-                   break;
+                    $validations = $this->config->item('form_status_actividad_usuario');
+                    break;
             }
             $this->form_validation->set_rules($validations); //Añadir validaciones
-            if ($this->form_validation->run() == TRUE)
-            {
+            if ($this->form_validation->run() == TRUE) {
                 $params = $this->input->post(null, true);
                 $params['id_usuario'] = $id_usuario;
                 $salida['tp_msg'] = $this->usuario->update($tipo, $params);
                 $output['status'] = $salida;
                 $salida['status'] = $salida['tp_msg'];
-            } else
-            {
+            } else {
                 $salida['tp_msg'] = En_tpmsg::DANGER;
                 $salida['msg'] = validation_errors();
                 $output['status'] = false;
             }
-            switch ($tipo)
-            {
+            switch ($tipo) {
                 case Usuario::BASICOS:
                     $view = $this->get_datos_basicos($id_usuario, $output);
                     break;
@@ -194,8 +179,7 @@ class Usuario extends MY_Controller
         echo json_encode($salida);
     }
 
-    private function get_datos_basicos($id_usuario = 0, &$output = [])
-    {
+    private function get_datos_basicos($id_usuario = 0, &$output = []) {
         $params['where'] = array(
             'usuarios.id_usuario' => $id_usuario
         );
@@ -208,35 +192,31 @@ class Usuario extends MY_Controller
             'inf.telefono_oficina', 'inf.email', 'dep.clave_departamental', 'cat.clave_categoria'
         );
         $resultado = $this->usuario->get_usuarios($params);
-        if (count($resultado) == 1)
-        {
+        if (count($resultado) == 1) {
             $output['usuario'] = $resultado[0];
         }
+        $output['language_text'] = $this->language_text;
         return $this->load->view('usuario/datos_basicos.tpl.php', $output, true);
     }
 
-    private function get_niveles($id_usuario = 0)
-    {
+    private function get_niveles($id_usuario = 0) {
         $output['grupos_usuario'] = $this->usuario->get_niveles_acceso($id_usuario);
         //pr($output);
         $output['view_grupos_usuario'] = $this->load->view('usuario/tabla_niveles.tpl.php', $output, true);
         return $this->load->view('usuario/niveles_acceso.tpl.php', $output, true);
     }
 
-    public function nuevo($tipo = Usuario::SIAP)
-    {
-        if ($this->input->post())
-        {
+    public function nuevo($tipo = Usuario::SIAP) {
+        if ($this->input->post()) {
             $this->config->load('form_validation'); //Cargar archivo con validaciones
-            $validations = $this->config->item('form_registro_'.$tipo); //Obtener validaciones de archivo general
+            $validations = $this->config->item('form_registro_' . $tipo); //Obtener validaciones de archivo general
             $this->form_validation->set_rules($validations); //Añadir validaciones
-            if ($this->form_validation->run() == TRUE)
-            {
+            if ($this->form_validation->run() == TRUE) {
                 $data = $this->input->post(null, true);
                 $data = $this->filtra_datos_form($data);
                 $output['post'] = $data;
                 $output['registro_valido'] = $this->usuario->nuevo($data, $tipo);
-            }else{
+            } else {
                 //pr($this->form_validation)
                 pr('no valido');
             }
@@ -244,15 +224,14 @@ class Usuario extends MY_Controller
         $this->load->model('Catalogo_model', 'catalogo');
         $this->load->model('Administracion_model', 'administrador');
         $output['tipo_registro'] = $tipo;
-        $output['delegaciones'] = dropdown_options($this->catalogo->get_delegaciones(null, array('oficinas_centrales'=>true)), 'clave_delegacional', 'nombre');
+        $output['delegaciones'] = dropdown_options($this->catalogo->get_delegaciones(null, array('oficinas_centrales' => true)), 'clave_delegacional', 'nombre');
         $output['nivel_atencion'] = dropdown_options($this->administrador->get_niveles_acceso(), 'id_grupo', 'nombre');
         $main_content = $this->load->view('usuario/nuevo.tpl.php', $output, true);
         $this->template->setMainContent($main_content);
         $this->template->getTemplate();
     }
 
-    private function filtra_datos_form($datos = [])
-    {
+    private function filtra_datos_form($datos = []) {
         $nuevo = [];
         foreach ($datos as $key => $value) {
             $nueva_llave = $key;
@@ -263,44 +242,36 @@ class Usuario extends MY_Controller
         return $nuevo;
     }
 
-    public function carga_usuarios()
-    {
+    public function carga_usuarios() {
         $view = [];
-        if ($this->input->post())
-        {     // SI EXISTE UN ARCHIVO EN POST
+        if ($this->input->post()) {     // SI EXISTE UN ARCHIVO EN POST
             $config['upload_path'] = './uploads/';      // CONFIGURAMOS LA RUTA DE LA CARGA PARA LA LIBRERIA UPLOAD
             $config['allowed_types'] = 'csv';           // CONFIGURAMOS EL TIPO DE ARCHIVO A CARGAR
             $config['max_size'] = '1000';               // CONFIGURAMOS EL PESO DEL ARCHIVO
             $this->load->library('upload', $config);    // CARGAMOS LA LIBRERIA UPLOAD
             $view['status']['result'] = false;
-            if ($this->upload->do_upload())
-            { //Se ejecuta la validación de datos
+            if ($this->upload->do_upload()) { //Se ejecuta la validación de datos
                 $this->load->library('csvimport');
                 $file_data = $this->upload->data();     //BUSCAMOS LA INFORMACIÓN DEL ARCHIVO CARGADO
                 $file_path = './uploads/' . $file_data['file_name'];         // CARGAMOS LA URL DEL ARCHIVO
 
-                if ($this->csvimport->get_array($file_path))
-                {              // EJECUTAMOS EL METODO get_array() DE LA LIBRERIA csvimport PARA BUSCAR SI EXISTEN DATOS EN EL ARCHIVO Y VERIFICAR SI ES UN CSV VALIDO
+                if ($this->csvimport->get_array($file_path)) {              // EJECUTAMOS EL METODO get_array() DE LA LIBRERIA csvimport PARA BUSCAR SI EXISTEN DATOS EN EL ARCHIVO Y VERIFICAR SI ES UN CSV VALIDO
                     $csv_array = $this->csvimport->get_array($file_path);   //SI EXISTEN DATOS, LOS CARGAMOS EN LA VARIABLE $csv_array
                     $tipo_registro = $this->input->post('tipo_registro', true);
                     $id_usuario = $this->get_datos_sesion('id_usuario');
                     $view['status'] = $this->usuario->carga_masiva($id_usuario, $tipo_registro, $file_data, $csv_array);
                     //pr($view['status']);
                     //$this->reporte_registro($view['status']);
-                } else
-                {
+                } else {
                     $view['status']['msg'] = "Formato inválido";
                 }
-            } else
-            {
+            } else {
                 $view['status']['msg'] = "Formato inválido";
             }
         }
-        if(isset($view['status']['result']) && $view['status']['result'])
-        {
+        if (isset($view['status']['result']) && $view['status']['result']) {
             redirect('precarga');
-        }else
-        {
+        } else {
             // pr($view);
             $main_content = $this->load->view('usuario/formulario_carga_v2.tpl.php', $view, true);
             $this->template->setMainContent($main_content);
@@ -308,14 +279,14 @@ class Usuario extends MY_Controller
         }
     }
 
-    private function reporte_registro(&$datos)
-   {
-       $filename = "Registro_" . date("d-m-Y_H-i-s") . ".xls";
-      /* header("Content-Type: application/vnd.ms-excel");
-       header("Content-Disposition: attachment; filename=$filename");
-       header("Pragma: no-cache");
-       header("Expires: 0");*/
-       echo $this->load->view('usuario/reporte_registro.tpl.php', $datos, TRUE);
-       exit();
-   }
+    private function reporte_registro(&$datos) {
+        $filename = "Registro_" . date("d-m-Y_H-i-s") . ".xls";
+        /* header("Content-Type: application/vnd.ms-excel");
+          header("Content-Disposition: attachment; filename=$filename");
+          header("Pragma: no-cache");
+          header("Expires: 0"); */
+        echo $this->load->view('usuario/reporte_registro.tpl.php', $datos, TRUE);
+        exit();
+    }
+
 }
