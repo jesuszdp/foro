30ce48b5064301dbc109a3d4d78038db05268b00
alejandra <alequiroz098@gmail.com>
2018-05-22 19:27:39 -0500

    links gestion revision
    links gestion revision

--- application/views/revision_trabajo_investigacion/estados/lista_sin_comite.php
+++ application/views/revision_trabajo_investigacion/estados/lista_sin_comite.php
@@ -54,6 +54,92 @@
   </tbody>
 </table>
 <!-- END lista sin comité -->
+
+      <!-- Modal 2 -->
+      <div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered"  role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h3 class="modal-title" id="exampleModalLabel2">Dictamen</h3>
+
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+
+              <table class="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Nombre del trabajo</th>
+                      <!-- <th scope="col">Puntua</th>
+                      <th scope="col">Opciones</th> -->
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">Lorem Ipsum ... </th>
+                      <!-- <td>Diabetes</td> -->
+                      <td>
+                        <!-- <button type="button" data-animation="flipInY" data-animation-delay="100" class="btn btn-theme btn-block submit-button" data-toggle="modal" data-target="#exampleModal">Asignar</button> -->
+                           <button type="button" data-animation="flipInY" data-animation-delay="100" class="btn btn-theme btn-block submit-button" data-toggle="dropdown">Cartel
+                           </button>
+                           <button type="button" data-animation="flipInY" data-animation-delay="100" class="btn btn-theme btn-block submit-button" data-toggle="dropdown">Ponencia
+                           </button>
+
+
+                      </td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Lorem Ipsum ...</th>
+                      <!-- <td>Medicina general</td> -->
+                      <td>
+                        <button type="button" data-animation="flipInY" data-animation-delay="100" class="btn btn-theme btn-block submit-button" data-toggle="dropdown">Cartel
+                        </button>
+                        <button type="button" data-animation="flipInY" data-animation-delay="100" class="btn btn-theme btn-block submit-button" data-toggle="dropdown">Ponencia
+                        </button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Lorem Ipsum ...</th>
+                      <!-- <td>Neurocirugia</td> -->
+                      <td>
+                        <button type="button" data-animation="flipInY" data-animation-delay="100" class="btn btn-theme btn-block submit-button" data-toggle="dropdown">Cartel
+                        </button>
+                        <button type="button" data-animation="flipInY" data-animation-delay="100" class="btn btn-theme btn-block submit-button" data-toggle="dropdown">Ponencia
+                        </button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Lorem Ipsum ...</th>
+                      <!-- <td>Neurocirugia</td> -->
+                      <td>
+                        <button type="button" data-animation="flipInY" data-animation-delay="100" class="btn btn-theme btn-block submit-button" data-toggle="dropdown">Cartel
+                        </button>
+                        <button type="button" data-animation="flipInY" data-animation-delay="100" class="btn btn-theme btn-block submit-button" data-toggle="dropdown">Ponencia
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                  </table>
+            </div>
+            <div class="modal-footer">
+              <!-- <button type="button" data-animation="flipInY" data-animation-delay="100" class="btn btn-theme btn-block submit-button" data-dismiss="modal">cerrar</button> -->
+              <button type="button" data-animation="flipInY" data-animation-delay="100" class="btn btn-theme btn-block" class="btn btn-primary">Guardar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Modal -->
+
+      <script type="text/javascript">
+          $('#myModal').on('shown.bs.modal', function () {
+          $('#myInput').trigger('focus')
+          })
+      </script>
+
+
+
 <script>
   $("#comite").addClass("active")
   $("#atencion").removeClass()
