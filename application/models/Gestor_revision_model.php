e15c1e4b52d968558e2e6b8c4ea74b4641804315
alejandra <alequiroz098@gmail.com>
2018-05-23 10:19:54 -0500

    modelo rechazados
    modelo rechazados

--- application/models/Gestor_revision_model.php
+++ application/models/Gestor_revision_model.php
@@ -11,7 +11,7 @@ class Gestor_revision_model extends MY_Model {
 
     /**
      * Devuelve la información de los registros de la tabla catalogos
-     * @author 
+     * @author
      * @date 21/05/2018
      * @return array
      */
@@ -22,7 +22,7 @@ class Gestor_revision_model extends MY_Model {
 
     /**
      * Devuelve la información de los registros de la tabla catalogos
-     * @author 
+     * @author
      * @date 21/05/2018
      * @return array
      */
@@ -33,7 +33,7 @@ class Gestor_revision_model extends MY_Model {
 
     /**
      * Devuelve la información de los registros de la tabla catalogos
-     * @author 
+     * @author
      * @date 21/05/2018
      * @return array
      */
@@ -45,7 +45,7 @@ class Gestor_revision_model extends MY_Model {
 
     /**
      * Devuelve la información de los registros de la tabla catalogos
-     * @author 
+     * @author
      * @date 21/05/2018
      * @return array
      */
@@ -57,7 +57,7 @@ class Gestor_revision_model extends MY_Model {
 
     /**
      * Devuelve la información de los registros de la tabla catalogos
-     * @author 
+     * @author
      * @date 21/05/2018
      * @return array
      */
@@ -69,7 +69,7 @@ class Gestor_revision_model extends MY_Model {
 
     /**
      * Devuelve la información de los registros de la tabla catalogos
-     * @author 
+     * @author
      * @date 21/05/2018
      * @return array
      */
@@ -81,19 +81,35 @@ class Gestor_revision_model extends MY_Model {
 
     /**
      * Devuelve la información de los registros de la tabla catalogos
-     * @author 
-     * @date 21/05/2018
+     * @author AlesSpock
+     * @date 22/05/2018
      * @return array
      */
     public function get_rechazados($param) {
-
-        $resut = [];
-        return $resut;
+        $this->db->flush_cache();
+        $this->db->reset_query();
+        $this->db->select(array(
+            "hr.folio folio", "ti.titulo titulo", "ma.lang metodologia",
+            "hr.clave_estado",
+            "CASE WHEN dn.tipo_exposicion='O' THEN 'Aceptado para exposición oral'
+            WHEN dn.tipo_exposicion='C' THEN 'Aceptado para exposición con cartel'
+            WHEN dn.tipo_exposicion='R' THEN 'Rechazado'
+            END tipo_exposicion", "rn.promedio_revision"
+        ));
+        $this->db->from("foro.historico_revision hr");
+        $this->db->join("foro.trabajo_investigacion ti ON hr.folio=ti.folio", 'left');
+        $this->db->join("foro.tipo_metodologia ma ON ti.id_tipo_metodologia=ma.id_tipo_metodologia", 'left');
+        $this->db->join("foro.revision rn ON hr.folio=rn.folio", 'left');
+        $this->db->join("foro.dictamen dn ON rn.folio=dn.folio", 'left');
+        $this->db->where_in("hr.clave_estado", ['aceptado_oral','aceptado_cartel']);
+        $this->db->where("actual", TRUE);
+        $result = $this->db->get();
+        return $resut->result_array();
     }
 
     /**
      * Devuelve la información de los registros de la tabla catalogos
-     * @author 
+     * @author
      * @date 21/05/2018
      * @return array
      */
