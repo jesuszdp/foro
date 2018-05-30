e6d6b6ea4e468cbb59ccbc49e008b66acfe49177
Sergio <cheko@MacBook-Pro-de-Sergio.local>
2018-05-30 13:17:11 -0500

    se paso a la version anterior de grocery crud

--- assets/grocery_crud/js/jquery_plugins/config/jquery.fancybox.config.js
+++ assets/grocery_crud/js/jquery_plugins/config/jquery.fancybox.config.js
@@ -1,11 +1,9 @@
-$(function() {
-    if ($('.image-thumbnail').length > 0) {
-        $('.image-thumbnail').fancybox({
-            'transitionIn': 'elastic',
-            'transitionOut': 'elastic',
-            'speedIn': 600,
-            'speedOut': 200,
-            'overlayShow': false
-        });
-    }
+$(function(){
+	$('.image-thumbnail').fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false
+	});	
 });	
